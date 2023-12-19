9601f27b77e9b26c5e9c5fd890cc9d4440d75e41
Wernisch, Marcel <marcel.wernisch@study.thws.de>
2023-12-19 22:18:34 +0100

    Fix Update of Stolperstein

--- app/ios/ARStolpersteine/ARStolpersteine/views/pages/tabviews/CameraARView.swift
+++ app/ios/ARStolpersteine/ARStolpersteine/views/pages/tabviews/CameraARView.swift
@@ -6,12 +6,84 @@
 //
 
 import SwiftUI
+import MapKit
 import RealityKit
+import OpenAPIRuntime
+import OpenAPIURLSession
 
-struct CameraARView: View {
+
+struct CameraARView: View, MapDelegate {
+    
+    // MARK: View Configuration
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.top)
     }
+    
+    // MARK: Code Section
+    private let client: Client
+    @State private var updatedRegion: MKCoordinateRegion? = nil
+    @State private var arPoints = [ARPoint]()
+    
+    init() {
+        self.client = Client(serverURL: try! Servers.server1(), transport: URLSessionTransport())
+        loadData()
+    }
+    
+    func loadData() {
+        if (self.updatedRegion != nil) {
+            Task {
+                try await loadStolpersteine()
+                updateAnchorView()
+            }
+        }
+    }
+  
+    
+    // MARK: AR-Point / Moving Code
+    func regionDidChanged(_ region: MKCoordinateRegion) {
+        NSLog("Region Changed")
+        self.updatedRegion = region
+        self.loadData()
+    }
+    
+    func loadStolpersteine() async throws {
+        NSLog("Load Stolpersteine")
+        let currentLocation = (long: self.updatedRegion!.center.longitude, lat: self.updatedRegion!.center.latitude)
+        
+        let response = try await client.getStolpersteineForUser(query: Operations.getStolpersteineForUser.Input.Query(radius: 0.002, latUser: Float(currentLocation.lat), lngUser: Float(currentLocation.long)))
+        switch response {
+        case let .ok(okResponse):
+            switch okResponse.body {
+            case .json(let json):
+                self.saveResult(json)
+            }
+        case .undocumented(statusCode: let statusCode, _):
+            NSLog("StatusCode: \(statusCode)")
+        case .badRequest(_):
+            NSLog("StatusCode: \(400)")
+        case .notFound(_):
+            NSLog("StatusCode: \(404)")
+        case .internalServerError(_):
+            NSLog("StatusCode: \(500)")
+        }
+    }
+    
+    private func saveResult(_ data: [Components.Schemas.StolpersteinPositionDto]) {
+        for stolpersteinPosition in data {
+            let arPoint = ARPoint(pin: CLLocationCoordinate2D(latitude: Double(stolpersteinPosition.position!.lat!), longitude: Double(stolpersteinPosition.position!.lng!)), data: stolpersteinPosition)
+            
+            self.arPoints.append(arPoint)
+        }
+    }
+    
+    
+    
+    // MARK: AR View Code
+    func updateAnchorView() {
+        
+    }
+    
+    
 }
 
 struct ARViewContainer: UIViewRepresentable {
@@ -36,10 +108,17 @@ struct ARViewContainer: UIViewRepresentable {
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
     
 }
 
+
+struct ARPoint {
+    var pin: CLLocationCoordinate2D
+    var data: Components.Schemas.StolpersteinPositionDto
+}
 
 #Preview {
     CameraARView()
