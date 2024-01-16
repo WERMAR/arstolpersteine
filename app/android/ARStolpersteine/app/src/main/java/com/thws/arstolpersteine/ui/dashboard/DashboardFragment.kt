d6080571a74c2c35d0a7bd55ed07ccd505d6815f
Yulia Fotieva <yulia.fotieva@study.thws.de>
2024-01-16 16:05:54 +0300

    request location added and current location

--- app/android/ARStolpersteine/app/src/main/java/com/thws/arstolpersteine/ui/dashboard/DashboardFragment.kt
+++ app/android/ARStolpersteine/app/src/main/java/com/thws/arstolpersteine/ui/dashboard/DashboardFragment.kt
@@ -7,17 +7,28 @@ package com.thws.arstolpersteine.ui.dashboard
 //import android.annotation.SuppressLint
 //import android.content.pm.PackageManager
 //import android.location.Location
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.location.LocationRequest
 import android.os.Bundle
 //import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 //import android.widget.Toast
 //import androidx.core.app.ActivityCompat
 //import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
 //import androidx.core.app.ActivityCompat.requestPermissions
 //import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
 //import com.example.arstolpersteine.R
 import com.thws.arstolpersteine.databinding.FragmentDashboardBinding
 //import com.example.arstolpersteine.ui.dashboard.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
@@ -31,6 +42,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 //import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polygon
 
 
 class DashboardFragment : Fragment(),
@@ -48,6 +60,9 @@ class DashboardFragment : Fragment(),
     //private var permissionDenied = false
     //private lateinit var map: GoogleMap
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    //for getting the last known location
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -55,17 +70,81 @@ class DashboardFragment : Fragment(),
         savedInstanceState: Bundle?
     ): View {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        //_binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        //val root: View = binding.root
+        //mapView = binding.mapView
+        //mapView?.onCreate(savedInstanceState)
+        //mapView?.getMapAsync(this)
+        //return root
+
+        val binding = FragmentDashboardBinding.inflate(inflater, container, false)
+
+        //binding.mapView.onCreate(savedInstanceState) эту и нижнюю строчки можно раскомментить
+        //binding.mapView.getMapAsync(this)
+        // Проверяем разрешение на доступ к точному местоположению
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Разрешение на доступ к точному местоположению уже предоставлено
+            // Вы можете продолжить с инициализацией карты
+            binding.mapView.onCreate(savedInstanceState)
+            binding.mapView.getMapAsync(this)
+        } else {
+            // Разрешение не предоставлено, запрашиваем его у пользователя
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
 
-        mapView = binding.mapView
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync(this)
+        return binding.root
 
-        return root
     }
 
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty()) {
+                when {
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED -> {
+                        // Разрешение на местоположение было предоставлено
+                        // Вы можете продолжить с инициализацией карты
+                        binding.mapView.onCreate(null)
+                        binding.mapView.getMapAsync(this)
+                    }
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                        // Пользователь отклонил разрешение, но ему уже показывалось запрос разрешения.
+                        // Вы можете предоставить дополнительные объяснения о том, почему нужно разрешение.
+                        // Например, показать диалоговое окно с объяснением и повторным запросом.
+                        binding.mapView.onCreate(null)
+                        binding.mapView.getMapAsync(this)
+                    }
+                    else -> {
+                        // Пользователь отклонил разрешение и не хочет больше видеть запрос разрешения.
+                        // Вы можете предоставить альтернативные варианты или принять меры в соответствии с вашей логикой.
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Внутри вашего фрагмента или активности добавьте константу для кода запроса разрешения
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 123
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -83,5 +162,37 @@ class DashboardFragment : Fragment(),
                 .position(thws)
                 .title("THWS campus"))
         googleMap?.moveCamera(CameraUpdateFactory.newLatLng(thws))
+
+        // Add a marker in thws and move the camera
+        val me = LatLng(13.778736483364845, 9.962629091249857)
+        googleMap?.addMarker(
+            MarkerOptions()
+                .position(me)
+                .title("here i am"))
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(me))
+
+// Get last known location
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    location?.let {
+                        // Add a marker at the last known location and move the camera
+                        val lastKnownLocation = LatLng(it.latitude, it.longitude)
+                        googleMap?.addMarker(
+                            MarkerOptions()
+                                .position(lastKnownLocation)
+                                .title("Last Known Location"))
+                        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(lastKnownLocation))
+                    }
+                }
+        } else {
+            // Handle the case where permission is not granted
+            // You might want to request permission or handle it according to your logic
+        }
+
     }
-}    
+}
